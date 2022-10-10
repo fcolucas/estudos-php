3b8d7164dbb012c9a9320fc710cb014568eafbb5
Paulo Henrique <paulo.henrique@vmetrix.com>
2022-10-10 15:56:54 -0300

    Hacktobefest

---
+++ array.php
@@ -0,0 +1,17 @@
+<?php
+$array = array(
+         "H",
+         "a",
+         "c",
+         "k",
+         "t",
+         "o",
+         "b",
+         "e",
+         "f",
+         "e",
+         "s",
+         "t",
+);
+var_dump($array);
+?>
